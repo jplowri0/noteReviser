eafbf362291d799440c63b08cca6adef92a1240b
jplow <jplow.tech@gmail.com>
2024-06-10 13:35:05 +1000

    stuff

--- checkNotes.py
+++ checkNotes.py
@@ -1,4 +1,20 @@
 
+
+# get a list of all files from fleeting , lit, and perm. Create a csv with SR,filename,count
+# Where count is the times this program has opened the file. 
+#
+# It needs to check for newly created files and add to list. 
+#
+# It will then randomly open a file, once opened it will add a 1 to the count. 
+# It cant reopen files until all have been opeened. 
+#
+# Files should be opened randomly. The file is opened by the SR. 
+#
+# When a file is opened, I want to see the filepath of the note and also the file contents.
+#
+# I should get an option to etihter go onto next note, Or follow the links. 
+
+
 def file_opener():
     # Define the path to your Markdown file
     file_path = 'filepath.txt'
